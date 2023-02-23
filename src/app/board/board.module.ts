c7c878ad3349acc20ba33c0868e71c1f1698b669
PawelPruchniak <pw.pruchniak@gmail.com>
2023-02-23 22:20:25 +0100

    Add navigation toolbar in board

--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BoardComponent } from './board.component';
 import { BoardRoutingModule } from './board-routing.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  imports: [BoardRoutingModule],
+  imports: [BoardRoutingModule, MatToolbarModule],
   declarations: [BoardComponent],
 })
 export class BoardModule {}
