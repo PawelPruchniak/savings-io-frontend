5281f09fb4d299d0af33fb9f9a8be94b5aeceb47
PawelPruchniak <pw.pruchniak@gmail.com>
2023-02-28 21:59:23 +0100

    Add balance card

--- src/app/account/main-account.module.ts
+++ src/app/account/main-account.module.ts
@@ -5,9 +5,18 @@ import { AsyncPipe, NgIf } from '@angular/common';
 import { SpinnerModule } from '../shared/spinner/spinner.module';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
-  imports: [MainAccountRoutingModule, NgIf, AsyncPipe, SpinnerModule, MatCardModule, MatProgressBarModule],
+  imports: [
+    MainAccountRoutingModule,
+    NgIf,
+    AsyncPipe,
+    SpinnerModule,
+    MatCardModule,
+    MatProgressBarModule,
+    MatDividerModule,
+  ],
   declarations: [MainAccountComponent],
 })
 export class MainAccountModule {}
