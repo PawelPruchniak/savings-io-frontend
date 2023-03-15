33e844a673041f6648381116552002b3130da76f
PawelPruchniak <pw.pruchniak@gmail.com>
2023-03-15 20:18:39 +0100

    Add showing investment result

--- src/app/account/account/account-card/investment-card/investment-card.component.ts
+++ src/app/account/account/account-card/investment-card/investment-card.component.ts
@@ -9,4 +9,40 @@ import { InvestmentAccount } from '@savings-io/shared/model';
 export class InvestmentCardComponent {
   @Input()
   investmentAccount!: InvestmentAccount;
+
+  get getTrendIcon(): string {
+    if (this.investmentAccount.investmentResultValue < 0) {
+      return 'trending_down';
+    }
+
+    if (this.investmentAccount.investmentResultValue > 0) {
+      return 'trending_up';
+    }
+
+    return 'trending_flat';
+  }
+
+  get getTrendStyleIconClass(): string {
+    if (this.investmentAccount.investmentResultValue < 0) {
+      return 'investment-icon-loss';
+    }
+
+    if (this.investmentAccount.investmentResultValue > 0) {
+      return 'investment-icon-gain';
+    }
+
+    return 'investment-icon-neutral';
+  }
+
+  get getTrendStyleValueClass(): string {
+    if (this.investmentAccount.investmentResultValue < 0) {
+      return 'investment-value-loss';
+    }
+
+    if (this.investmentAccount.investmentResultValue > 0) {
+      return 'investment-value-gain';
+    }
+
+    return 'investment-value-neutral';
+  }
 }
--- src/app/account/account/account-card/investment-card/investment-card.module.ts
+++ src/app/account/account/account-card/investment-card/investment-card.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { InvestmentCardComponent } from './investment-card.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [MatCardModule, MatListModule],
+  imports: [MatCardModule, MatListModule, MatIconModule],
   declarations: [InvestmentCardComponent],
   exports: [InvestmentCardComponent],
 })
